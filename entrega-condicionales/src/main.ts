f609c622a99d6dd1581751e16ed40f840eb5037e
Alvaro <alvarolopezfernandez1992@gmail.com>
2024-11-03 12:07:17 +0100

    Ajustada entrega-condicionales según criterios de evaluación

--- entrega-condicionales/src/main.ts
+++ entrega-condicionales/src/main.ts
@@ -1,10 +1,10 @@
 import "./style.css";
-import { cardSource } from "./cards";
+
+type Estado = "Has ganado" | "Has perdido" | "Seguir jugando";
 
 // Variables;
 
 let userScore: number = 0;
-let selectedCard: { url: string; value: number };
 
 // Enlaces a elementos HTML;
 
@@ -12,86 +12,128 @@ const gameMessage = document.getElementById("gameStatus");
 const getCardButton = document.getElementById("getCard");
 const standButton = document.getElementById("stand");
 const newGameButton = document.getElementById("newGame");
-const showCard = document.getElementById("gameScore");
+const score = document.getElementById("gameScore");
 const currentCard = document.getElementById("currentCard");
 const knowOutComeButton = document.getElementById("knowOutCome");
 
 // Función que devuelve un número aleatorio;
 
 const getRandomNumber = (): number => {
-  return Math.floor(Math.random() * 12) + 1;
+  return Math.floor(Math.random() * 10) + 1;
 };
 
 // Función que ajusta el número si es mayor que 7 sumando +2;
 
 const adjustNumber = (num: number): number => {
-  return num > 7 && num < 10 ? num + 2 : num;
+  return num > 7 ? num + 2 : num;
 };
 
-// Funcion que muestra la puntuación total en el HTML;
+// Funcion que nos devuelve la url de la carta
 
-const updateScoreDisplay = (): void => {
-  if (showCard instanceof HTMLElement) {
-    showCard.textContent = `Score: ${userScore}`;
+const getCardUrl = (cardNumber: number): string => {
+  console.log(cardNumber);
+
+  switch (cardNumber) {
+    case 1:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg";
+    case 2:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg";
+    case 3:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg";
+    case 4:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg";
+    case 5:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg";
+    case 6:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg";
+    case 7:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg";
+    case 10:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg";
+    case 11:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg";
+    case 12:
+      return "https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg";
+
+    default:
+      return "URL no disponible";
   }
 };
 
-// Funcion que devuelve la url de la carta elegida;
+// Función que muestra la carta en el html
 
-const getCardUrl = (): string => {
-  const randomNumber = getRandomNumber();
-  const adjustedNumber = adjustNumber(randomNumber);
+const showCardUrl = (card: string) => {
+  console.log(card);
 
-  const card = cardSource.find((card) => {
-    const regex = new RegExp(`/${adjustedNumber}_`);
-    return regex.test(card.url);
-  });
-
-  if (card) {
-    selectedCard = card;
-    return card.url;
-  }
-  throw new Error("No se encontró la carta");
-};
-
-// Funcion que muestra la carta seleccionada en el HTML;
-
-const displayCard = (): void => {
-  const cardUrl = getCardUrl();
-  if (currentCard instanceof HTMLImageElement) {
-    currentCard.src = cardUrl;
+  if (
+    currentCard !== undefined &&
+    currentCard !== null &&
+    currentCard instanceof HTMLImageElement
+  ) {
+    currentCard.src = card;
   }
 };
 
 // Funcion que calcula el valor de la carta seleccionada;
 
-const selectedCardValue = (): number => {
-  return selectedCard.value;
+const selectedCardValue = (num: number): number => {
+  if (num > 7) {
+    return (num = 0.5);
+  } else return num;
 };
 
-// Funcion que suma el valor de las cartas;
+// Función que suma los puntos
+const sumPoints = (cardValue: number): number => {
+  userScore += cardValue;
+  return userScore;
+};
+// Funcion que muestra la puntuación total en el HTML;
 
-const sumCardValue = (): number => {
-  return userScore + selectedCardValue();
+const updateScoreDisplay = (totalPoints: number): void => {
+  if (score !== null && score !== undefined && score instanceof HTMLElement) {
+    score.innerHTML = `Puntuación: ${totalPoints}`;
+  }
 };
 
-// Funcion que actualiza userScore;
+// Función que controla el estado de la partida
 
-const updateUserScore = () => {
-  userScore = sumCardValue();
-  updateScoreDisplay();
+const checkGameState = (): Estado => {
+  let state: Estado = "Seguir jugando";
+  if (userScore === 7.5) state = "Has ganado";
+  else if (userScore > 7.5) state = "Has perdido";
+  return state;
 };
 
 // Función que comprueba si hemos ganado o no la partida;
 
-const checkGameStatus = (): void => {
-  if (userScore === 7.5 && gameMessage != null) {
-    gameMessage.textContent = "!Has ganado¡";
-    toggleButtons(true);
-  } else if (userScore > 7.5 && gameMessage != null) {
-    gameMessage.textContent = "!Has perdido¡";
-    toggleButtons(true);
+const manageGameStatus = (state: string): void => {
+  switch (state) {
+    case "Has ganado":
+      if (gameMessage !== undefined && gameMessage !== null) {
+        gameMessage.innerHTML = `!Enhorabuena¡ Has ganado con ${userScore} puntos`;
+      }
+      toggleButtons(true);
+      break;
+    case "Has perdido":
+      if (gameMessage !== undefined && gameMessage !== null) {
+        gameMessage.innerHTML = `!Has perdido¡ Te has pasado con ${userScore} puntos`;
+      }
+      toggleButtons(true);
   }
+};
+
+// Funcion que controla el flujo del juego
+
+const handleGame = (): void => {
+  const randomNumber = getRandomNumber();
+  const adjustedNumber = adjustNumber(randomNumber);
+  const card = getCardUrl(adjustedNumber);
+  const cardValue = selectedCardValue(adjustedNumber);
+  const totalPoints = sumPoints(cardValue);
+  showCardUrl(card);
+  updateScoreDisplay(totalPoints);
+  const gameState = checkGameState();
+  manageGameStatus(gameState);
 };
 
 // Función que habilita o deshabilita los botones;
@@ -109,17 +151,8 @@ const toggleButtons = (disable: boolean): void => {
   }
 };
 
-// Eventos
-
-// Cuando el jugador pulsa "Pedir carta"
-
-getCardButton?.addEventListener("click", () => {
-  displayCard();
-  updateUserScore();
-  checkGameStatus();
-});
-
 // Funcionalidad botón plantarse
+
 const stand = () => {
   if (gameMessage) {
     gameMessage.textContent = `Te has plantado con ${userScore} puntos`;
@@ -130,57 +163,12 @@ const stand = () => {
     knowOutComeButton.style.display = "inline-block";
   }
 };
-// Evento cuando el jugador pulsa "Plantarse"
-
-standButton?.addEventListener("click", stand);
-
-// Funcionalidad botón saber que habría pasado
-
-const knowOutCome = () => {
-  let simulatedScore = userScore;
-  let outComeMessage = "";
-
-  const updateSimulatedUserScore = () => {
-    simulatedScore = sumCardValue();
-    updateScoreDisplay();
-  };
-
-  if (simulatedScore < 7.5) {
-    displayCard();
-    simulatedScore += selectedCardValue();
-    updateSimulatedUserScore();
-
-    if (simulatedScore === 7.5) {
-      outComeMessage = "!Habrías ganado¡";
-    }
-
-    if (simulatedScore > 7.5) {
-      outComeMessage = `!Te habrías pasado con ${simulatedScore} puntos¡`;
-    }
-  }
-
-  if (!outComeMessage) {
-    outComeMessage = `!Te habrías quedado con ${simulatedScore} puntos¡`;
-  }
-
-  if (gameMessage) {
-    gameMessage.textContent = outComeMessage;
-  }
-
-  if (knowOutComeButton) {
-    knowOutComeButton.style.display = "none";
-  }
-};
-
-// Evento botón Saber que habría pasado.
-
-knowOutComeButton?.addEventListener("click", knowOutCome);
 
 // Funcionalidad botón nueva partida
 
 const newGame = () => {
   userScore = 0;
-  updateScoreDisplay();
+  updateScoreDisplay(0);
   toggleButtons(false);
   if (currentCard instanceof HTMLImageElement) {
     currentCard.src =
@@ -198,6 +186,64 @@ const newGame = () => {
     gameMessage.textContent = "Comienza a Jugar";
   }
 };
+
+// Funcionalidad botón saber que habría pasado
+
+const simulatedCardDrawing = (): { cardValue: number; cardUrl: string } => {
+  const randomNumber: number = getRandomNumber();
+  const adjustedNumber: number = adjustNumber(randomNumber);
+  const cardValue = selectedCardValue(adjustedNumber);
+  const cardUrl = getCardUrl(adjustedNumber);
+  return { cardValue, cardUrl };
+};
+
+const knowOutCome = () => {
+  const { cardValue, cardUrl } = simulatedCardDrawing();
+  let simulatedScore: number = userScore + cardValue;
+
+  const simulatedState: Estado =
+    simulatedScore === 7.5
+      ? "Has ganado"
+      : simulatedScore > 7.5
+      ? "Has perdido"
+      : "Seguir jugando";
+
+  switch (simulatedState) {
+    case "Has ganado":
+      if (gameMessage !== undefined && gameMessage !== null) {
+        gameMessage.innerHTML = `Habrías ganado con ${simulatedScore} puntos`;
+      }
+    case "Has perdido":
+      if (gameMessage !== undefined && gameMessage !== null) {
+        gameMessage.innerHTML = `Habrías perdido con ${simulatedScore} puntos`;
+      }
+    case "Seguir jugando":
+      if (gameMessage !== undefined && gameMessage !== null) {
+        gameMessage.innerHTML = `Te habrías quedado con ${simulatedScore} puntos`;
+      }
+  }
+
+  showCardUrl(cardUrl);
+  if (knowOutComeButton) {
+    knowOutComeButton.style.display = "none";
+  }
+};
+
+// Eventos
+
+// Cuando el jugador pulsa "Pedir carta"
+
+getCardButton?.addEventListener("click", () => {
+  handleGame();
+});
+
+// Evento cuando el jugador pulsa "Plantarse"
+
+standButton?.addEventListener("click", stand);
+
+// Evento botón Saber que habría pasado.
+
+knowOutComeButton?.addEventListener("click", knowOutCome);
 
 // Evento cuando el jugador pulsa "New Game"
 
